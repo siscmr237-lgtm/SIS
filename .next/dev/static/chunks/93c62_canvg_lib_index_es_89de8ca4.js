2e7d31f2f9c854d10aa7aeb7ee0beee2323b7a2e
siscmr237-lgtm <siscmr237@gmail.com>
2026-07-18 19:29:58 +0100

    .next issues
    .next issues

---
+++ .next/dev/static/chunks/93c62_canvg_lib_index_es_89de8ca4.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/93c62_7efe5e26._.js"
+],
+    source: "dynamic"
+});
