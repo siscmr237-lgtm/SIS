a0584c28d4b88ac9ac78682f4b5dedc551dbaf82
siscmr237-lgtm <siscmr237@gmail.com>
2026-07-18 19:16:12 +0100

    load time
    load time

---
+++ .next/dev/static/chunks/OneDrive_Desktop_Sis_Code_SIS_app_page_tsx_777f8f44._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/93c62_0c7c00b0._.js",
+  "static/chunks/OneDrive_Desktop_Sis_Code_SIS_2d6f387d._.js",
+  "static/chunks/93c62_tailwind-merge_dist_bundle-mjs_mjs_21152558._.js",
+  "static/chunks/93c62_pako_dist_pako_esm_mjs_940f2a84._.js",
+  "static/chunks/93c62_jspdf_dist_jspdf_es_min_736382c6.js",
+  "static/chunks/93c62_f69a7cb5._.js"
+],
+    source: "dynamic"
+});
